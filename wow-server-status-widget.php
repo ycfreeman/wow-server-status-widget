ba51816018c1eb8ac346079a3b97b57859b23c86
Freeman Man <freeman@ycfreeman.com>
2015-11-01 03:57:24 +1000

    update license info

--- wow-server-status-widget.php
+++ wow-server-status-widget.php
@@ -7,22 +7,7 @@
  * Author: Freeman Man
  * Author URI: http://www.ycfreeman.com
  */
-/**
- * Copyright (C) 2010  Freeman, (Yu Chung) Man (email : ycfreeman@yahoo.com.hk)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/.
- */
+
 /**
  * help and bug report url as well as icons url for easier maintainence
  */
